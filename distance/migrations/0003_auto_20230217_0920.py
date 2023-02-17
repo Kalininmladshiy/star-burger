4ad6f9bec1d008b87b29e68c98109d0c5c843a2b
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-02-17 12:34:13 +0300

    Added coordinates to model place

---
+++ distance/migrations/0003_auto_20230217_0920.py
@@ -0,0 +1,23 @@
+from django.db import migrations
+from restaurateur.utils import fetch_coordinates
+
+
+def add_lat_lon(apps, schema_editor):
+    Place = apps.get_model('distance', 'Place')
+    for place in Place.objects.all():
+        lat, lon = fetch_coordinates(place.address)
+        place.lat = lat
+        place.save()
+        place.lon = lon
+        place.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('distance', '0002_auto_20230217_0854'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_lat_lon),
+    ]
