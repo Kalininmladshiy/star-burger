c1dab2521154995ec5e7923d7543461329d02d51
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-02-27 16:18:48 +0300

    Made the fields required

--- distance/models.py
+++ distance/models.py
@@ -6,14 +6,12 @@ class Place(models.Model):
     address = models.CharField(
         'Адрес места',
         max_length=50,
-        blank=True,
         unique=True,
     )
     lat = models.FloatField(verbose_name='широта', null=True, blank=True)
     lon = models.FloatField(verbose_name='долгота', null=True, blank=True)
     requested_at = models.DateTimeField(
         'Время запроса координат',
-        null=True,
         db_index=True,
         default=timezone.now,
     )
